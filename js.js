08373333fc661d5c43c193804f5e533c3ac40942
mahdi <mmddrid@gmail.com>
2023-11-29 21:55:08 +0100

    updatt

--- js.js
+++ js.js
@@ -21,6 +21,9 @@ let sub = document.getElementById('sub');
 // console.log(tiltle,price,taxes,ads,discunt,count,total,category,sub)
 
 //get total
+
+let mode = 'creat';
+let tmp;
 function getTotal()
 {
      if(price.value != '')
@@ -62,16 +65,26 @@ sub.onclick = function(){
       count:count.value,
       category:category.value
    }
-   if(newpro.count > 1)
+   if (mode === "create")
    {
-      for(let i = 0; i < newpro.count; i++)
+
+      if(newpro.count > 1)
       {
-            datapro.push(newpro);
+         for(let i = 0; i < newpro.count; i++)
+         {
+               datapro.push(newpro);
+         }
+      }else
+      {
+         datapro.push(newpro);
+   
       }
    }else
    {
-      datapro.push(newpro);
-
+      datapro[tmp]=newpro
+      mode = 'create';
+      sub.innerHTML = 'Create';
+      count.style.display='block';
    }
 
    localStorage.setItem('product',JSON.stringify(datapro))
@@ -111,7 +124,7 @@ function showDate()
          <td>${datapro[i].ads}</td>
          <td>${datapro[i].discunt}</td>
          <td>${datapro[i].category}</td>
-         <td><button id="update">Update</button></td>
+         <td><button  onclick="updateDate(${i})" id="update">Update</button></td>
          <td><button  onclick="deletedata(${i})" id="delte"> delete</button></td>
       </tr>
       `
@@ -121,7 +134,7 @@ function showDate()
    if(datapro.length >0)
    {
       btndelte.innerHTML = `
-     <button onclick="daletall">delet All(${datapro.length})</button>
+     <button onclick="daletall()" >delet All(${datapro.length})</button>
       `
    }else
    {
@@ -132,6 +145,8 @@ showDate()
 
 function deletedata(i)
 {
+
+   getTotal();
    datapro.splice(i,1)
    localStorage.product = JSON.stringify(datapro)
    showDate()
@@ -142,4 +157,92 @@ function daletall()
    localStorage.clear()
    datapro.splice(0)
    showDate()
+}
+
+
+function updateDate(i)
+{
+  tiltle.value = datapro[i].tiltle
+  price.value = datapro[i].price
+  taxes.value = datapro[i].taxes
+  ads.value = datapro[i].ads
+  getTotal()
+count.style.display = "none"
+  category.value = datapro[i].category
+  sub.innerHTML = 'Update'
+  mode = "update"
+  tmp = i;
+  scroll(
+   {
+      top:0,
+      behavior:'smooth',
+   }
+  )
+ }
+
+let searchMood = 'title';
+function getSearchmode(id)
+{
+   let search= document.getElementById('sreach')
+
+   if(id == 'sreachtitel')
+   {
+      searchMood = 'title'
+      search.placeholder = 'search By Title';
+   }else
+   {
+      searchMood = 'categoory'
+      search.placeholder = 'search By Category ';
+   }
+   search.focus()
+   console.log(searchMood)
+}
+
+function searchData(value)
+{
+   if(searchMood == 'title')
+   {
+      let table = ' ';
+      for(let i = 0; i < datapro.length;i++)
+      {
+         if(datapro[i].tiltle.toLowerCase.includes(value))
+         {
+            table += `
+               <tr>
+                  <td>${i}</td>
+                  <td>${datapro[i].tiltle}</td>
+                  <td>${datapro[i].price}</td>
+                  <td>${datapro[i].taxes}</td>
+                  <td>${datapro[i].ads}</td>
+                  <td>${datapro[i].discunt}</td>
+                  <td>${datapro[i].category}</td>
+                  <td><button  onclick="updateDate(${i})" id="update">Update</button></td>
+                  <td><button  onclick="deletedata(${i})" id="delte"> delete</button></td>
+               </tr>
+            `
+         }
+      }
+   }else
+   {
+      for(let i = 0; i <datapro.length;i++)
+      {
+         if(datapro[i].category.includes(value.toLowerCase()))
+         {
+            table += `
+               <tr>
+                  <td>${i}</td>
+                  <td>${datapro[i].tiltle}</td>
+                  <td>${datapro[i].price}</td>
+                  <td>${datapro[i].taxes}</td>
+                  <td>${datapro[i].ads}</td>
+                  <td>${datapro[i].discunt}</td>
+                  <td>${datapro[i].category}</td>
+                  <td><button  onclick="updateDate(${i})" id="update">Update</button></td>
+                  <td><button  onclick="deletedata(${i})" id="delte"> delete</button></td>
+               </tr>
+            `
+         }
+      }
+   }
+   document.getElementById('tbody').innerHTML = table;
 }
